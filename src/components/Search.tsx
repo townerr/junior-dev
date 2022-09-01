4e81323727b4171936bad69642356a9048c2819d
Ryan <townerr@yahoo.com>
2022-09-01 10:21:52 -0500

    init commit + homescree design/implem + some jobs page design

---
+++ src/components/Search.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <nav className='flex flex-row p-2 gap-4 w-full bg-purple-600 items-center'>
+      <div>
+        <p className='font-mono text-2xl px-4 font-semibold text-white'>
+          Junior.Dev
+        </p>
+      </div>
+      <div className='w-full'>
+        <input className='rounded bg-gray-100 text-2xl p-2 w-full focus:outline-none' type="text" placeholder="Search for a job or keyword" />
+      </div>
+    </nav>
+  )
+}
+
+export default Search
