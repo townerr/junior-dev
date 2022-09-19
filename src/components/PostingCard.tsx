0208b1e8c8c0e52c4bd4c31a76037369a17709fb
Ryan Towner <townerr@yahoo.com>
2022-09-19 18:28:32 -0500

    added routing to card component to get to a job posting

--- src/components/PostingCard.tsx
+++ src/components/PostingCard.tsx
@@ -1,12 +1,18 @@
 
 import React from 'react';
 import { Posting } from '../types/Posting';
+import { useRouter } from 'next/router';
 
 //TODO: redirect to the posting page of /[id]
 
 const PostingCard = ({id, employer, employerIng, location, title, type, salary, createdAt} : Posting) => {
+  const router = useRouter();
+  const handleClick = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    router.push(`/${id}`);
+  }
   return (
-    <a href="#" className='m-4 flex flex-row bg-gray-200 w-[23rem] max-h-32 rounded-2xl px-8 py-6 gap-4 items-center hover:scale-[1.02] hover:bg-gray-300'>
+    <a href={id} onClick={handleClick} className='m-4 flex flex-row bg-gray-200 w-[23rem] max-h-32 rounded-2xl px-8 py-6 gap-4 items-center hover:scale-[1.02] hover:bg-gray-300'>
       <img src={employerIng} alt={employer} width={64} height={64} />
       <div>
         <h1 className='font-semibold text-lg'>{title}</h1>
