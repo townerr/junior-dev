f65ea1cd51c1a32794c46bdbe2d06820c40b06d0
Ryan Towner <townerr@yahoo.com>
2022-09-20 13:11:34 -0500

    added font awesome and icons for text logo in search bar

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLessThan, faGreaterThan } from '@fortawesome/free-solid-svg-icons';
 
 const Search = () => {
   return (
     <nav className='flex flex-row p-2 gap-4 w-full bg-purple-600 items-center'>
       <div>
-        <a href="/jobs" className='font-mono text-2xl px-4 font-semibold text-white'>
-          Junior.Dev
+        <a href="/jobs" className='font-mono text-2xl px-4 font-semibold text-white flex flex-row items-center gap-2'>
+          <FontAwesomeIcon width={18} icon={faLessThan} /> Junior.Dev <FontAwesomeIcon width={18} icon={faGreaterThan} />
         </a>
       </div>
       <div className='w-full'>
